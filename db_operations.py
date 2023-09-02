cad1cdb04b0eb4d4968b78b5a228badc95ff69b1
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 12:17:29 -0300

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 # Read environment variables
-telegram_api_key = os.environ.get("6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2IY")
+telegram_api_key = os.environ.get("6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY")
 database_uri = os.environ.get("DATABASE_URI")
 
 # Connect to the PostgreSQL database
