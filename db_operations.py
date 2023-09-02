b8cf37c1baceb943f55c0af2b22f4b4c76f9224f
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 17:54:56 -0300

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -2,13 +2,12 @@ import os
 import psycopg2
 
 # Read environment variables
-telegram_api_key = os.environ.get("6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY")
+telegram_api_key = os.environ.get("TELEGRAM_API_KEY")
 database_uri = os.environ.get("DATABASE_URI")
 
 # Connect to the PostgreSQL database
 try:
-    conn = psycopg2.connect(postgres://epusmlirevfjmr:2fed1e715c73d95006c8e2be06b8b3858367e0ef6e9108096cf767af4a28b96d@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d31e8rvj3ul73q
-)
+    conn = psycopg2.connect(postgres://epusmlirevfjmr:2fed1e715c73d95006c8e2be06b8b3858367e0ef6e9108096cf767af4a28b96d@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d31e8rvj3ul73q)
 except Exception as e:
     print(f"Error: Unable to connect to the database. {e}")
     exit(1)
