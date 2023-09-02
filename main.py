ee83c585e67a6538b41a923ff4c8ca754b2eecd2
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 12:44:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from telegram.ext import Updater, CommandHandler
 
 # Initialize the Updater
 token = os.environ.get("TELEGRAM_BOT_TOKEN")  # Make sure to set this env variable in Heroku
-updater = Updater(token=token, use_context=True)
 
-# Your other code here...
+# Debugging line to print the token, remove it later
+print(f"Token: {token}")
 
-# Start the Bot
-port = int(os.environ.get("PORT", 8443))  # Also set the PORT env variable in Heroku
-updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+if token is None:
+    print("Token is not set!")
+else:
+    updater = Updater(token=token, use_context=True)
 
+    # Your other code here...
+
+    # Start the Bot
+    port = int(os.environ.get("PORT", 8443))  # Also set the PORT env variable in Heroku
+    updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
