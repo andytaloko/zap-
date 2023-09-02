97e5189c844b1cbccac219855fc5d40966465483
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 11:30:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ updater = Updater(token=settings.TELEGRAM_API_KEY)
 updater.start_webhook(listen="0.0.0.0",
                       port=port,
                       url_path=settings.TELEGRAM_API_KEY)
-updater.bot.set_webhook("https://zap-corretor.herokuapp.com/" + settings.TELEGRAM_API_KEY)
+updater.bot.set_webhook("https://https://zap-corretor-21afd1e16ec3.herokuapp.com/.herokuapp.com/" + settings.TELEGRAM_API_KEY)
