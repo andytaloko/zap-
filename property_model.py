23bd78ba3de55d3d226ab8d83c88907a24eb28a0
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 11:14:57 -0300

    Update property_model.py

--- property_model.py
+++ property_model.py
@@ -3,3 +3,18 @@ def start(update, context):
 
 def list_property(update, context):
     update.message.reply_text('Listing properties...')
+
+class Property:
+    def __init__(self, id, location, features):
+        self.id = id
+        self.location = location
+        self.features = features
+
+from telegram.ext import Updater, CommandHandler
+import settings
+
+updater = Updater(token=settings.TELEGRAM_API_KEY)
+
+# Add handlers and other functionalities here
+
+updater.start_polling()
