a1c1561b12b094ec988f6b7e4514d4ea65ce4f7e
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 11:23:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from telegram.ext import Updater, CommandHandler
+import os
 import settings
+
+# Read port from environment variables
+port = int(os.environ.get('PORT', 5000))
 
 updater = Updater(token=settings.TELEGRAM_API_KEY)
 
 # Add handlers and other functionalities here
 
-updater.start_polling()
+# For Heroku Deployment
+updater.start_webhook(listen="0.0.0.0",
+                      port=port,
+                      url_path=settings.TELEGRAM_API_KEY)
+updater.bot.set_webhook("https://your-heroku-app-name.herokuapp.com/" + settings.TELEGRAM_API_KEY)
