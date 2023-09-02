5e4bb9a3ff956c7ce4a551abdd9870fd22c15248
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 11:55:53 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-TELEGRAM_API_KEY ='6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I'
-DATABASE_URI ='postgres://epusmlirevfjmr:2fed1e715c73d95006c8e2be06b8b3858367e0ef6e9108096cf767af4a28b96d@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d31e8rvj3ul73q'
+TELEGRAM_API_KEY = '6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I'
+DATABASE_URI = 'postgres://epusmlirevfjmr:2fed1e715c73d95006c8e2be06b8b3858367e0ef6e9108096cf767af4a28b96d@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d31e8rvj3ul73q'
