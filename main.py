7c2223a234223d43887c1ed98f73df13d5958e93
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 12:19:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ updater = Updater(token=os.environ.get("6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8C
 
 # rest of your code
 
-import os
-
-from telegram.ext import Updater
-import settings
-
-updater = Updater(token=settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
-
 # Your handlers here
 
 port = int(os.environ.get('PORT', '8443'))
