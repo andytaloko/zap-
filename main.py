1247da0f0ae23563d5962c9dc1b37261f370fd52
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 11:39:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 from telegram.ext import Updater
 import settings
 
-updater = Updater(token=settings.TELEGRAM_API_KEY)
+updater = Updater(token=settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
 
 # Your handlers here
 
 port = int(os.environ.get('PORT', '8443'))
 updater.start_webhook(listen="0.0.0.0",
                       port=port,
-                      url_path=settings.TELEGRAM_API_KEY)
+                      url_path=settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
 updater.bot.set_webhook("https://your-heroku-app-name.herokuapp.com/" + settings.TELEGRAM_API_KEY)
