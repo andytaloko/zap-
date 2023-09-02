4692119547cc7b569e3c610297bbee8cb57f38d0
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 12:15:57 -0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler
 def start(update, context):
     update.message.reply_text('Hello, welcome to zapCORRETOR!')
 
-updater = Updater(token='6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I', use_context=True)
+updater = Updater(token="6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY", use_context=True)
 
 dp = updater.dispatcher
 
