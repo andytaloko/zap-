7de8f17bf7be46d0e07e3c9df56589f528b576aa
andytaloko <andrewmatthews7@gmail.com>
2023-09-02 12:13:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from telegram.ext import Updater
 
-updater = Updater(token=os.environ.get("6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2IY"))
+updater = Updater(token=os.environ.get("6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY"))
 
 # rest of your code
 
@@ -17,5 +17,5 @@ updater = Updater(token=settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
 port = int(os.environ.get('PORT', '8443'))
 updater.start_webhook(listen="0.0.0.0",
                       port=port,
-                      url_path=settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
-updater.bot.set_webhook("https://zap-corretor-21afd1e16ec3.herokuapp.com/" + settings.6372799289:AAHNFkVjfCBvllvQE7p8U4sdHpSl2wgge2I)
+                      url_path=settings.("6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY")
+updater.bot.set_webhook("https://zap-corretor-21afd1e16ec3.herokuapp.com/" + settings."6548277251:AAG2PMddqTTSRIyCIkxitm2C_Jo8CiIF0hY")
